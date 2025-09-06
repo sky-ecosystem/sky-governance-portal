af9cdceaa037fb7941ef62ae8622dd3f9175f763
tyler17 <tylerya@gmail.com>
2025-09-05 19:32:10 -0700

    add pagination to the allDelegations query

--- modules/delegates/api/fetchDelegationMetrics.ts
+++ modules/delegates/api/fetchDelegationMetrics.ts
@@ -7,7 +7,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 */
 
 import { gqlRequest } from 'modules/gql/gqlRequest';
-import { allDelegations } from 'modules/gql/queries/subgraph/allDelegations';
+import { allDelegationsPaginated } from 'modules/gql/queries/subgraph/allDelegations';
 import { SupportedNetworks } from 'modules/web3/constants/networks';
 import { networkNameToChainId } from 'modules/web3/helpers/chain';
 import { formatEther } from 'viem';
@@ -17,14 +17,44 @@ interface DelegationMetrics {
   delegatorCount: number;
 }
 
+interface Delegation {
+  delegator: string;
+  delegate: {
+    id: string;
+    version: string;
+  };
+  amount: string;
+}
+
 export async function fetchDelegationMetrics(network: SupportedNetworks): Promise<DelegationMetrics> {
-  const res = await gqlRequest<any>({
-    chainId: networkNameToChainId(network),
-    query: allDelegations
-  });
-  const delegations = res.delegations;
-  const totalSkyDelegated = formatEther(delegations.reduce((acc, cur) => acc + BigInt(cur.amount), 0n));
-  const delegatorCount = delegations.filter(d => d.amount > 0n).length;
+  const chainId = networkNameToChainId(network);
+  const pageSize = 1000;
+  let skip = 0;
+  let hasMore = true;
+  const allDelegations: Delegation[] = [];
+
+  // Fetch all delegations using pagination
+  while (hasMore) {
+    const res = await gqlRequest<any>({
+      chainId,
+      query: allDelegationsPaginated,
+      variables: {
+        first: pageSize,
+        skip
+      }
+    });
+
+    const delegations = res.delegations || [];
+    allDelegations.push(...delegations);
+
+    // Check if there are more results to fetch
+    hasMore = delegations.length === pageSize;
+    skip += pageSize;
+  }
+
+  // Calculate metrics from all delegations
+  const totalSkyDelegated = formatEther(allDelegations.reduce((acc, cur) => acc + BigInt(cur.amount), 0n));
+  const delegatorCount = allDelegations.filter(d => BigInt(d.amount) > 0n).length;
 
   return {
     totalSkyDelegated,
